5815d8bb527c7da872d89f952bb02c9849b8aa1d
Priscilla Okawa <prisoka@gmail.com>
2019-03-08 14:53:30 -0800

    added failing tests for binarySearch()

---
+++ test/binarySearchTests.js
@@ -0,0 +1,21 @@
+"use strict"
+
+const expect = require("chai").expect
+
+describe(("binary search tests"), function(){
+  it(("binarySearch() should be a function"), function(){
+    expect(binarySearch).to.be.a("function")
+  })
+
+  it(("binarySearch() should return the index of the given target"), function(){
+    expect(binarySearch([1,2,3,4,5], 2)).to.equal(1)
+  })
+
+  it(("binarySearch() should return -1 if the given target is not found"), function(){
+    expect(binarySearch([1,2,3,4,5], 7)).to.equal(-1)
+  })
+
+  it(("binarySearch() should return -1 if the given target is in a non-sorted array"), function(){
+    expect(binarySearch([1,7,2,3,4,5], 7)).to.equal(-1)
+  })
+})
